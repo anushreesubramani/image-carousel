df99eee97cfe1a47f92039fea6dfe5923c9dfe00
Anushree Subramani <anushree3791@gmail.com>
2018-09-06 14:54:20 +0530

    Added breakpoints.js, moved api key to secrets.js

---
+++ src/breakpoints.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    device: 'desktop',
+    minWidth: 769,
+    maxWidth: 1024,
+    slidesToShow: 5
+  },
+  {
+    device: 'tablet',
+    breakpoint: 600,
+    minWidth: 577,
+    maxWidth: 768,
+    slidesToShow: 2
+  },
+  {
+    device: 'mobile',
+    breakpoint: 480,
+    minWidth: 0,
+    maxWidth: 576,
+    slidesToShow: 1
+  }
+];
